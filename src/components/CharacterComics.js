48323bd0ef918c35922a6020b236c0b40c4fe586
Verchenko Maxim <verchenko1998@gmail.com>
2020-10-15 12:57:34 +0300

    Character comics (1.1)

--- src/components/CharacterComics.js
+++ src/components/CharacterComics.js
@@ -10,7 +10,7 @@ const CharacterComics = (props) => {
   const getComics = async () => {
     try {
       const data = await fetch(
-        `https://gateway.marvel.com:443/v1/public/characters/${props.id}/comics?ts=1&apikey=349e7f8260f2e901f46c4bb2d3de202b&hash=b7d357b46149f83f432b1aee8834d8a`
+        `https://gateway.marvel.com:443/v1/public/characters/${props.id}/comics?ts=1&apikey=349e7f8260f2e901f46c4bb2d3de202b&hash=b7d357b46149f83f432b1aee8834d8a3`
       )
         .then((res) => res.json())
         .catch((err) => console.log(err));
